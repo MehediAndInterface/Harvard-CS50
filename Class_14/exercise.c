ba3dd894cfc9589ec2426717fcd40d103c5886ce
Mehedi <mehedi.interface@gmail.com>
2025-04-29 23:17:03 +0300

    class 14 exercise

--- Class_14/exercise.c
+++ Class_14/exercise.c
@@ -7,27 +7,28 @@ Write a C program to print even numbers from 2 to 10 using all three loops.
 int main() {
     int i;
 
-    printf("Using for loop:\n");
+    printf("Using for loop:\n"); 
     for (i = 2; i <= 10; i += 2) {
-        printf("%d ", i);
+        printf("%d ", i); 
     }
-    printf("\n\n");
+    printf("\n\n"); 
 
-    printf("Using while loop:\n");
-    i = 2;
+    printf("Using while loop:\n"); 
+    i = 2; 
     while (i <= 10) {
         printf("%d ", i);
-        i += 2;
+        i += 2;  
     }
-    printf("\n\n");
+    printf("\n\n"); 
 
-    printf("Using do-while loop:\n");
-    i = 2;
+    printf("Using do-while loop:\n"); 
+
+    i = 2; 
     do {
-        printf("%d ", i);
-        i += 2;
-    } while (i <= 10);
-    printf("\n");
+        printf("%d ", i); 
+        i += 2; 
+    } while (i <= 10); 
+    printf("\n\n"); 
 
     return 0;
 }
